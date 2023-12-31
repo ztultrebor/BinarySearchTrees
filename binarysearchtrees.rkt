163bb175643a772d3a85f68c1f0a6b454d897251
Robert Lutz <robertlutzi@gmail.com>
2023-12-31 18:42:39 -0500

    refactor rebalance using abstract function

--- binarysearchtrees.rkt
+++ binarysearchtrees.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname binarysearchtrees) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname binarysearchtrees) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
 ; data definitions
 
@@ -24,6 +24,82 @@
 ; ==========================
 ; functions
 
+(define (create-bst kvpair)
+  ; Number X -> BST
+  ; creates a sungle-node BST with key and val
+  (make-bst (first kvpair) (second kvpair) #f #f))
+
+
+(define (create-bst-from-list kvlist)
+  ; [ListOf [Number X]] -> BST
+  ; creates a (probably unbalanced) BST from a list of key value pairs
+  (foldr insert-node #f (map create-bst kvlist)))
+
+
+(define (insert-node node tree)
+  ; BST BST -> BST
+  ; inserts a new node into a tree in the correct leaf,
+  ; but without rebalancing the tree
+  (cond
+    [(false? tree) node]
+    [(false? node) tree]
+    [else (cond
+            [(< (bst-key node) (bst-key tree))
+             (make-bst
+              (bst-key tree) (bst-val tree)
+              (insert-node node (bst-lbranch tree)) (bst-rbranch tree))]
+            [(> (bst-key node) (bst-key tree))
+             (make-bst
+              (bst-key tree) (bst-val tree) (bst-lbranch tree)
+              (insert-node node (bst-rbranch tree)))]
+            [(= (bst-key node) (bst-key tree)) tree])]))
+
+
+(define (rebalance tree)
+  ; BST -> BST
+  ; rebalances the tree when the branches become uneven
+  (local (
+          (define (purge key tree)
+            ; Natural BST -> BST
+            ; purge a node with given key,
+            ; along with all its subbranches from tree
+            (cond
+              [(false? tree) #f]
+              [(= (bst-key tree) key) #f]
+              [(< key (bst-key tree))
+               (make-bst (bst-key tree) (bst-val tree)
+                         (purge key (bst-lbranch tree)) (bst-rbranch tree))]
+              [(> key (bst-key tree))
+               (make-bst (bst-key tree) (bst-val tree)
+                         (bst-lbranch tree) (purge key (bst-rbranch tree)))]))
+          (define (reorganizer new-root old-main pruner)
+            ; BST BST BST -> BST
+            (rebalance
+             (insert-node old-main
+                          (insert-node
+                           (purge (bst-key new-root) pruner) 
+                           new-root)))))
+    ; - IN -
+    (cond
+      [(false? tree) #f]
+      [(> (- (count-nodes (bst-rbranch tree))
+             (count-nodes (bst-lbranch tree))) 1)
+       (reorganizer (leftmost (bst-rbranch tree))
+                    (make-bst (bst-key tree) (bst-val tree) (bst-lbranch tree) #f)
+                    (bst-rbranch tree))]
+      [(> (- (count-nodes (bst-lbranch tree))
+             (count-nodes (bst-rbranch tree))) 1)
+       (reorganizer (rightmost (bst-lbranch tree))
+                    (make-bst (bst-key tree) (bst-val tree) #f (bst-rbranch tree))
+                    (bst-lbranch tree))]
+      [else (make-bst (bst-key tree) (bst-val tree)
+                      (rebalance (bst-lbranch tree))
+                      (rebalance (bst-rbranch tree)))])))
+
+
+
+
+     
 (define (lookup key tree)
   ; Natural BST -> Any
   ; search a BST for the provided key and return the
@@ -44,56 +120,6 @@
     [else (append (inorder (bst-lbranch tree))
                   (list (bst-val tree))
                   (inorder (bst-rbranch tree)))]))
-
-
-(define (add-to-tree node tree)
-  ; BST BST -> BST
-  ; adds a new node into a tree and rebalances it
-  (rebalance (insert-node node tree)))
-
-
-(define (insert-node node tree)
-  ; BST BST -> BST
-  ; inserts a new node into a tree in the correct leaf,
-  ; but without rebalancing the tree
-  (cond
-    [(false? tree) node]
-    [else (cond
-            [(< (bst-key node) (bst-key tree))
-             (make-bst
-              (bst-key tree) (bst-val tree)
-              (insert-node node (bst-lbranch tree)) (bst-rbranch tree))]
-            [(> (bst-key node) (bst-key tree))
-             (make-bst
-              (bst-key tree) (bst-val tree) (bst-lbranch tree)
-              (insert-node node (bst-rbranch tree)))]
-            [(= (bst-key node) (bst-key tree)) tree])]))
-
-
-(define (rebalance tree)
-  ; BST -> BST
-  ; rebalances the tree when the branches become uneven
-  (cond
-    [(false? tree) #f]
-    [(> (- (count-nodes (bst-lbranch tree))
-           (count-nodes (bst-rbranch tree))) 1)
-     (rebalance (purge-duplicates
-                 (insert-node
-                  (make-bst (bst-key tree) (bst-val tree)
-                            #f (bst-rbranch tree))
-                  (insert-node (bst-lbranch tree)
-                               (rightmost (bst-lbranch tree))))))]
-    [(> (- (count-nodes (bst-rbranch tree))
-           (count-nodes (bst-lbranch tree))) 1)
-     (rebalance (purge-duplicates
-                 (insert-node
-                  (make-bst (bst-key tree) (bst-val tree)
-                            (bst-lbranch tree) #f)
-                  (insert-node (bst-rbranch tree)
-                               (leftmost (bst-rbranch tree))))))]
-    [else (make-bst (bst-key tree) (bst-val tree)
-                    (rebalance (bst-lbranch tree))
-                    (rebalance (bst-rbranch tree)))]))
 
 
 (define (count-nodes tree)
@@ -121,70 +147,29 @@
     [else (leftmost (bst-lbranch tree))]))
 
 
-(define (purge-duplicates tree)
-  ; BST BST -> BST
-  ; eliminate any duplicates of the root node of tree
-  (make-bst (bst-key tree) (bst-val tree)
-            (purge (bst-key tree) (bst-lbranch tree))
-            (purge (bst-key tree) (bst-rbranch tree))))
-
-
-(define (purge key tree)
-  ; Natural BST -> BST
-  ; delete node with given key from tree
-  (cond
-    [(false? tree) #f]
-    [(= (bst-key tree) key)
-     (cond
-       [(false? (bst-lbranch tree)) (bst-rbranch tree)]
-       [(false? (bst-rbranch tree)) (bst-lbranch tree)]
-       [else tree])]
-    [(< key (bst-key tree))
-     (make-bst (bst-key tree) (bst-val tree)
-               (purge key (bst-lbranch tree)) (bst-rbranch tree))]
-    [(> key (bst-key tree))
-     (make-bst (bst-key tree) (bst-val tree)
-               (bst-lbranch tree) (purge key (bst-rbranch tree)))]))
-
-  
 ; ===========================
 ; checks
-(define BST1 (make-bst 1 "m" false false))
-(define BST2 (make-bst 2 "x" false false))
-(define BST3 (make-bst 3 "w" false false))
-(define BST4 (make-bst 4 "k" false false))
-(define BST5 (make-bst 5 "g" false false))
-(define BST7 (make-bst 7 "p" false false))
-(define twee (insert-node
-              BST7
-              (insert-node
-               BST5
-               (insert-node
-                BST4
-                (insert-node BST3 (insert-node BST2 BST1))))))
-(check-expect (lookup 2 twee) "x")
-(check-expect (lookup 6 twee) #f)
-(check-expect (insert-node BST1 #f) BST1)
-(check-expect (insert-node BST4 BST1) (make-bst 1 "m" false BST4))
-(check-expect (insert-node BST7 (insert-node BST4 BST1))
-              (make-bst 1 "m" false (make-bst 4 "k" false BST7)))
-(check-expect (rebalance (insert-node BST7 (insert-node BST4 BST1)))
-              (make-bst 4 "k" (make-bst 1 "m"  #f #f) (make-bst 7 "p" #f #f)))
-(check-expect (add-to-tree
-                BST4
-                (rebalance (insert-node
-                            BST7
-                            (insert-node
-                             BST5
-                             (insert-node BST3 (insert-node BST1 BST2))))))
-              (rebalance twee))
-(check-expect (count-nodes (insert-node BST7 (insert-node BST4 BST1))) 3)
-(check-expect (purge 1 BST1) #f)
-(check-expect (purge 1 (insert-node BST1 BST3)) BST3)
-(check-expect (purge 1 (insert-node BST3 BST1)) BST3)
-(check-expect (purge 1
-                     (insert-node (insert-node BST4 (insert-node BST1 BST3))
-                                  (insert-node BST7 BST5)))
-              (insert-node BST7 (insert-node BST4
-                                             (insert-node BST3 BST5))))
-(check-expect (inorder twee) '("m" "x" "w" "k" "g" "p"))
+
+(define kvlist (create-bst-from-list '((25 x) (13 x) (20 x) (10 x) (7 x)
+                                              (1 x) (15 x) (4 x) (9 x))))
+(define dogma (create-bst-from-list '((99 o) (77 l) (24 i) (10 h) (95 g)
+                                              (15 d) (89 c) (29 b) (63 a))))
+(check-expect (create-bst '(5 x)) (make-bst 5 'x #f #f))
+(check-expect (create-bst-from-list '((5 m))) (make-bst 5 'm #f #f))
+(define babi-list-pt1 (create-bst-from-list '((1 x) (2 x))))
+(define babi-list-pt2 (create-bst-from-list '((5 x) (4 x) (3 x))))
+(define merged-babi (create-bst-from-list '((5 x) (4 x) (3 x) (1 x) (2 x))))
+(define searchy-babi (create-bst-from-list '((1 x) (5 x) (4 x) (2 x) (3 x))))
+(check-expect (insert-node babi-list-pt2 babi-list-pt1) merged-babi)
+(check-expect (rebalance babi-list-pt2) (create-bst-from-list
+                                         '((5 x) (3 x) (4 x))))
+(check-expect (rebalance searchy-babi) searchy-babi)
+(check-expect (rebalance merged-babi) searchy-babi)
+
+
+
+(rebalance kvlist)
+
+(rebalance dogma)
+
+dogma
